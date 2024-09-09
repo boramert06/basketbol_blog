c4db11dbf526f1aa681ce78a5aa00a5fc9c478f7
Bora Mert Karademir <98692526+boramert06@users.noreply.github.com>
2024-09-10 00:10:47 +0300

    update

--- Deneme/obj/Debug/net8.0/Deneme.AssemblyInfo.cs
+++ Deneme/obj/Debug/net8.0/Deneme.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Deneme")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+394aa5ac2b050a1225dcd8e394905d69dfd2d031")]
 [assembly: System.Reflection.AssemblyProductAttribute("Deneme")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Deneme")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
